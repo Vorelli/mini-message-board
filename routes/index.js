2c851cee89c81c7194fe53aabde015f41cb04f20
Natale Toscano <kickalliance195@gmail.com>
2020-06-25 16:57:34 -0400

    Finished project. All messages are stored in memory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+const title = 'Mini Messageboard';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: title, messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: title });
+});
+
+router.post('/new', function (req, res, next) {
+  let formData = req.body;
+  messages.push({
+    text: formData.text,
+    user: formData.user,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
